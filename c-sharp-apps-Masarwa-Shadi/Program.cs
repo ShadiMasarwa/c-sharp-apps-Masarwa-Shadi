40036e203eb027195d60b336cbae171c735cbf19
ShadiMasarwa <masarwa@gmail.com>
2024-05-17 20:10:24 +0300

    Add project files.

---
+++ c-sharp-apps-Masarwa-Shadi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_Masarwa_Shadi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
