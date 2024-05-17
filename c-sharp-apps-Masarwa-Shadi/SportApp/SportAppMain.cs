9a18168fe43e7cca2dd201072149152857d820d1
ShadiMasarwa <masarwa@gmail.com>
2024-05-17 20:18:49 +0300

    1st commit on task1 branch

---
+++ c-sharp-apps-Masarwa-Shadi/SportApp/SportAppMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_Masarwa_Shadi.SportApp
+{
+    internal class SportAppMain
+    {
+        public static void MainEntry()
+        {
+            Console.WriteLine("SportApp");
+        }
+    }
+}
