076dde609cafb90a825c38057a17aff34246f72e
ShadiMasarwa <96420608+ShadiMasarwa@users.noreply.github.com>
2024-05-24 09:14:48 +0300

    public-static

--- c-sharp-apps-Masarwa-Shadi/Program.cs
+++ c-sharp-apps-Masarwa-Shadi/Program.cs
@@ -8,7 +8,7 @@ namespace c_sharp_apps_Masarwa_Shadi
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
