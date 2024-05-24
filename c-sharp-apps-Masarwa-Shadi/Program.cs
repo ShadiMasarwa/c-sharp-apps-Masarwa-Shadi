0f7e4a66a3a315cda9e83273d0a1fe0083cca7ce
ShadiMasarwa <96420608+ShadiMasarwa@users.noreply.github.com>
2024-05-24 09:09:37 +0300

    public - internal

--- c-sharp-apps-Masarwa-Shadi/Program.cs
+++ c-sharp-apps-Masarwa-Shadi/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace c_sharp_apps_Masarwa_Shadi
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
