392edec62c737019e37f41bc4d1aa782e4c737df
visual <visual@hermelin.ort.org.il>
2024-06-07 09:20:29 +0300

    Sport App 1

---
+++ c-sharp-apps-Masarwa-Shadi/SportApp/League.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_Masarwa_Shadi.SportApp
+{
+    class League
+    {
+    }
+}
